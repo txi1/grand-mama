0d7e2654e2d3e460dda5e5d4bc9d93a7a4adee99
Taylor <txi1@ocdsb.ca>
2019-11-02 17:00:01 -0400

    Added textfield to input name of classroom

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -24,15 +26,29 @@ public class Main extends Application{
             closeProgram();
             });
 
-
-            //Setup for the intro menu
+            //Setup for intro menu
+            GridPane menuLayout = new GridPane();
+            menuLayout.setPadding(new Insets(10,10,10,10));
+            menuLayout.setVgap(8);
+            menuLayout.setHgap(10);
+            
             Label label1 = new Label("Select Classroom");
+            menuLayout.setConstraints(label1, 1, 0);
+
+
             Button button1 = new Button("Enter this classroom");
+            menuLayout.setConstraints(button1, 1, 1);
             button1.setOnAction(e -> mainWindow.setScene(classMenu));
+
+            Button makeClass = new Button("Make a new Classroom");
+            menuLayout.setConstraints(makeClass, 1, 2);
+            makeClass.setOnAction(e -> System.out.println(createClassroom.display("Class","Classroom Creation")));
+        
+            menuLayout.getChildren().addAll(label1, button1, makeClass);
+
         
             //Layout configuration for the intro menu and adding the elements to the menu
-            VBox menuLayout = new VBox(20);
-            menuLayout.getChildren().addAll(label1, button1);
+            
             firstMenu = new Scene(menuLayout, 200, 200);
 
             //Setup for classroom menu
@@ -45,10 +61,7 @@ public class Main extends Application{
             classMenu = new Scene(classLayout, 400, 300);
 
         //Button that will pop up the classroom creation window
-        Button makeClass = new Button("Make a new Classroom");
-        makeClass.setOnAction(e -> createClassroom.display("Class","Classroom Creation"));
         
-        menuLayout.getChildren().addAll(makeClass);
 
         mainWindow.setScene(firstMenu);
         mainWindow.setTitle("mainMenu");
